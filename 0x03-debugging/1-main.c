348466e546c1441e7a27174954a85c1aa2049ad4
xioperez01 <angie.xioperez@gmail.com>
2020-06-17 15:23:35 +0000

    again

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,6 @@
 
 int main(void)
 {
-<<<<<<< HEAD
   int i;
 
   printf("Infinite loop incoming :(\n");
@@ -22,20 +21,4 @@ int main(void)
   printf("Infinite loop avoided! \\o/\n");
 
   return (0);
-=======
-int i;
-
-printf("Infinite loop incoming :(\n");
-
-i = 0;
-/**
- *while (i < 10) - infinite loop
- *{
- *putchar(i);
- *}
- */
-printf("Infinite loop avoided! \\o/\n");
-
-return (0);
->>>>>>> 3d7d54caf4316980b186e6e3dd2e9c49083e20c9
 }
