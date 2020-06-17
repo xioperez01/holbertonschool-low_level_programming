7077dfd8f98810e7fb69801b17d059b0be84c159
xioperez01 <angie.xioperez@gmail.com>
2020-06-17 01:25:39 +0000

    code

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_to_98 -Entry point
+ *@n: variable
+ *prints 0 to 98
+ */
+void print_to_98(int n)
+{
+  int m;
+  if (n < 98)
+    {
+      for (m = n; 
