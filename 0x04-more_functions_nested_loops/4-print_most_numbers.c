34f209267c064e4fe4da3b121b2dc2b8dc27586d
xioperez01 <angie.xioperez@gmail.com>
2020-06-18 22:35:40 +0000

    code

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - prints from 0 to 9 whitout 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char n = '0';
+
+	while (n <= '9')
+	{
+		if ((n != '2') && (n != '4'))
+			{
+				_putchar(n);
+			}
+			n++;
+	}
+		_putchar('\n');
+}
