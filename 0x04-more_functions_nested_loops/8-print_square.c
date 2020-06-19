751677770138ad62dc45a396169951190a7f79d6
xioperez01 <angie.xioperez@gmail.com>
2020-06-19 02:18:07 +0000

    lines

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,9 +11,9 @@ void print_square(int size)
 	{
 		_putchar('\n');
 	}
-	for (r = 0; r <= size; r++)
+	for (r = 1; r <= size; r++)
 	{
-		for (c = 0; c <= size; c++)
+		for (c = 1; c <= size; c++)
 		{
 			_putchar('#');
 		}
