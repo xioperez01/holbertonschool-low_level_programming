e9a9d7e710977cdeef2b15f59d9bffcfdcef9569
xioperez01 <angie.xioperez@gmail.com>
2020-06-19 03:02:04 +0000

    code

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,23 +7,21 @@ int main(void)
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf("FizzBuzz");
-			printf(" ");
-
 		}
 		else if (n % 3 == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else if (n % 5 == 0)
 		{
 			printf("Buzz");
-			printf(" ");
-
 		}
 		else
 		{
 			printf("%d", n);
+		}
+		if (n < 100)
+		{
 			printf(" ");
 		}
 	}
