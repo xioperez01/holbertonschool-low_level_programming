d5cb5baee987c31564688d11938938966e6213b8
xioperez01 <angie.xioperez@gmail.com>
2020-06-23 21:33:10 +0000

    header and executable

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *swap_int - swaps the values of two integers.
+ *@a: integer
+ *@b: integer
+ */
+void swap_int(int *a, int *b)
+{
+	int change;
+
+	change = *a;
+	*a = *b;
+	*b = change;
+}
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
  *_puts - prints a string
  *@str: string
