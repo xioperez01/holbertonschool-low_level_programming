b8b8ae569edd0b4dac5f878cb4a243802fb2a6df
xioperez01 <angie.xioperez@gmail.com>
2020-06-25 22:21:54 +0000

    change for

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,14 +12,16 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	for (; dest[i] != '\0'; i++)
-		while (src[j] != src[n])
-		{
-			dest[i] = src[i];
-			i++;
-			j++;
-		}
-		dest[i] = '\0';
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != src[n])
+	{
+		dest[i] = src[i];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 
 	return (dest);
 }
