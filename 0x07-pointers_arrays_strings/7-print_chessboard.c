6198ec1d5a95ae915c306aa785c2b2aa7d768933
xioperez01 <angie.xioperez@gmail.com>
2020-06-29 23:34:54 +0000

    code

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *print_chessboard - prints the chessboard.
+ *@a: where the characters of the chessboard
+ *Return: Void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; i++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
