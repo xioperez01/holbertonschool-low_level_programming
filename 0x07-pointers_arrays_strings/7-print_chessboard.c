e1601df753f5cb60dcee36d48791f7b35b30d698
xioperez01 <angie.xioperez@gmail.com>
2020-06-29 23:41:41 +0000

    second for

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,6 @@
 /**
  *print_chessboard - prints the chessboard.
  *@a: where the characters of the chessboard
- *Return: Void
  */
 void print_chessboard(char (*a)[8])
 {
@@ -10,7 +9,7 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 8; i++)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
