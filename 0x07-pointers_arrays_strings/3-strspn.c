a71783e1f977d9f053ff464f01e62f3d7af1a781
xioperez01 <angie.xioperez@gmail.com>
2020-06-30 00:55:06 +0000

    suspect

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,13 +13,13 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
 			{
-				if (s[i] == accept[j])
-				{
-					n++;
-					break;
-				}
+				n++;
+				break;
 			}
+		}
 		if (accept[j] == '\0')
 		{
 			return (n);
