6229772c70c4c6e23cca6d43f79a293be417af0a
xioperez01 <angie.xioperez@gmail.com>
2020-07-06 19:55:41 +0000

    code

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+	int i, tot;
+	int j;
+
+	if (argc > 1)
+	{
+		for(i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j]; j++)
+			{
+				if( !isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			tot = tot + atoi(argv[i]);
+		}
+
+	}
+	else
+	{
+		printf("0\n");
+		return(0);
+	}
+
+	printf("%i\n", tot);
+	return(0);
+}
