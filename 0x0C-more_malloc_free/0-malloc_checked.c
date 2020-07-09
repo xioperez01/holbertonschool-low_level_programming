3a3c36587dccdd81d453d3007ca30bbf58d59120
xioperez01 <angie.xioperez@gmail.com>
2020-07-09 17:09:44 +0000

    initial code

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ *malloc_checked - allocates memory and exit
+ *@b: size of the memory solicited
+ *Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(int) * b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
