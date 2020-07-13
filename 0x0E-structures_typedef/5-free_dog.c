c2b3112d4372a7dfe9dff9e8ccf03c550a705109
xioperez01 <angie.xioperez@gmail.com>
2020-07-13 23:05:18 +0000

    with NULL again

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -11,9 +11,9 @@ void free_dog(dog_t *d)
 	if (d == NULL)
 		return;
 
-	if ((d->name))
+	if ((d->name) != NULL)
 		free(d->name);
 
-	if ((d->owner))
+	if ((d->owner) != NULL)
 		free(d->owner);
 }
