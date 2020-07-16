ecb31b986575026e85edb62a94e40ef2d12665dd
xioperez01 <angie.xioperez@gmail.com>
2020-07-16 00:42:49 +0000

    initial doc

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+/**
+ *op_add - returns the sum of a and b
+ *@a: integer
+ *@b: integer
+ *Return: a integer with the result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - returns the difference of a and b
+ *@a: integer
+ *@b: integer
+ *Return: a integer with the result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - returns the product of a and b
+ *Return: a integer with the result
+ *@a: integer
+ *@b: integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - returns result of thr division of a and b
+ *@a: integer
+ *@b: integer
+ *Return: a integer with the result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - returns the remainder of the division ofa and b
+ *@a: integer
+ *@b: integer
+ *Return: a integer with the result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
