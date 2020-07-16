25c1a837794137e93d69cbb3c2add27caee3b975
xioperez01 <angie.xioperez@gmail.com>
2020-07-16 00:52:31 +0000

    initial doc

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,28 @@
+#ifndef PROTOTYPES_H
+#define PROTOTYPES_H
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+
+/* functions of operations */
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/* function that selects the correct function */
+int (*get_op_func(char *s))(int, int);
+
+#endif
