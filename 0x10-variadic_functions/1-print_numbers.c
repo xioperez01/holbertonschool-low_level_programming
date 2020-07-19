24e7b782207cc7606f50774acb7c4a78002bb1cf
xioperez01 <angie.xioperez@gmail.com>
2020-07-19 16:13:20 +0000

    erasing first if

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,9 +13,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list variable;
 	unsigned int i = 0;
 
-	if (separator == NULL)
-		return;
-
 	if (n > 0)
 	{
 		va_start(variable, n);
