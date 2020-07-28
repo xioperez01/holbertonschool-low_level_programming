961f73162b575562679c8703863589790a43e6f1
xioperez01 <angie.xioperez@gmail.com>
2020-07-28 19:24:56 +0000

    Initial code task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *free_listint - a function that frees a list_t list.
+ *@head: list
+ *Return Void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *pointer;
+	while (head != NULL)
+	{
+		pointer = head;
+		head = head->next;
+		free(pointer);
+	}
+}
