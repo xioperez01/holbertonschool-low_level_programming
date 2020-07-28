570782e8ebacb755b960433f745d392197571ba3
xioperez01 <angie.xioperez@gmail.com>
2020-07-28 23:14:22 +0000

    Adding head validation! = Null in to for

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	for (counter = 0; counter < index; counter++)
+	for (counter = 0; counter < index && head != NULL; counter++)
 		head = head->next;
 
 	if (counter == index)
