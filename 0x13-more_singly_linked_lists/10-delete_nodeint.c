3a67a0774e244754e105a54d13b45ba3f4b94ed2
xioperez01 <angie.xioperez@gmail.com>
2020-07-29 03:30:50 +0000

    temp2

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	temp2 = temp->next;
-	temp = (temp->next)->next;
+	temp = temp2->next;
 	free(temp2);
 	return (1);
 }
