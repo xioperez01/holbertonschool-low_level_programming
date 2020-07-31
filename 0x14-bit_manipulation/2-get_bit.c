931752d3472f35c828484673ce7c9a7f306dfdd3
xioperez01 <angie.xioperez@gmail.com>
2020-07-31 22:53:17 +0000

    Adding length limit of long

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#define MAX_LEN (sizeof(long int) * 8)
 /**
  *get_bit -  a function that returns the value of a bit at a given index.
  *@n: Number in base 2
@@ -9,7 +9,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (n >= index)
+	if (index <= MAX_LEN)
 	{
 		n = n >> index;
 		index = (n & 1);
