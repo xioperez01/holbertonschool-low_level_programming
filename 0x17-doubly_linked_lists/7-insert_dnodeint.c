67f6ed1f5afc1286e2100eade500813a2a514e46
xioperez01 <angie.xioperez@gmail.com>
2020-09-07 02:37:38 +0000

    Adding index = 0 condition

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	tmp = *h;
 	new_node->n = n;
 
+	if (idx == 0)
+	{
+		new_node->prev = NULL;
+		tmp->prev = new_node;
+		new_node->next = tmp;
+		*h = new_node;
+		return (new_node);
+	}
+	
 	while (new_node)
 	{
 		if (i == idx)
