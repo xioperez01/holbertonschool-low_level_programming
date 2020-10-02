a9ef7557f5f510dd7460b3ed3ab261f0f48bf623
Angie Pérez <66028978+xioperez01@users.noreply.github.com>
2020-10-02 15:57:04 -0500

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 	hash_node_t **array;
-	unsigned int i = 0;
+	unsigned long int i = 0;
 
 	if (size == 0)
 		return (NULL);
