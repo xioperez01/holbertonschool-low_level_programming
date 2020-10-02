80759551c4942d668f6fab89b9206e8bf5a1f63c
Angie Pérez <66028978+xioperez01@users.noreply.github.com>
2020-10-02 16:04:54 -0500

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (!new_table)
 		return (NULL);
-	array = malloc(sizeof(*hash_node_t) * size);
+	array = malloc(sizeof(*array) * size);
 
 	if (!array)
 	{
