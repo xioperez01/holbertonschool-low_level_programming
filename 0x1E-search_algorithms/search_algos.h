5dff716c2450df06763926071f8c7e6e84cc40b3
xioperez01 <angie.xioperez@gmail.com>
2021-01-14 01:16:03 +0000

    Advanced task

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,11 +5,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t l, size_t r); /** Auxiliar **/
+int jump_search(int *array, size_t size, int value); /** Advanced **/
 
 #endif
