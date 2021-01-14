f3905dd69a5d9d72b30ab98393f9a7c229f684c8
xioperez01 <angie.xioperez@gmail.com>
2021-01-14 22:14:31 +0000

    Initial code task 10 (Advanced)

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,5 +12,5 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t l, size_t r); /** Auxiliar **/
 int jump_search(int *array, size_t size, int value); /** Advanced **/
-
+int interpolation_search(int *array, size_t size, int value); /** Advanced **/
 #endif
