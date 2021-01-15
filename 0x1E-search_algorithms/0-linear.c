b916d75a0ae91cc5f9bfd09661d2e4fcedb50c57
xioperez01 <angie.xioperez@gmail.com>
2021-01-15 00:31:50 +0000

    With out size conditional

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,12 +13,12 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (!array | (size == 0))
+	if (!array)
 		return (-1);
 
 	while (i < size)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 		i++;
