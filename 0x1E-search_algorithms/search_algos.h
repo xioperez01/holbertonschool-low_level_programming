06b503ab83c219e637944d5cac51d1d82de8673f
xioperez01 <angie.xioperez@gmail.com>
2021-01-15 15:58:12 +0000

    Betty stylle

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,10 +10,15 @@
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print_array(int *array, size_t l, size_t r); /** Auxiliar **/
-int jump_search(int *array, size_t size, int value); /** Advanced **/
-int interpolation_search(int *array, size_t size, int value); /** Advanced **/
-int auxiliar_search(int array[], int start, int end, int key); /* Auxiliar */
-int exponential_search(int *array, size_t size, int value); /* Advanced */
+/* Auxiliar */
+void print_array(int *array, size_t l, size_t r);
+/* Advanced */
+int jump_search(int *array, size_t size, int value);
+/* Advanced */
+int interpolation_search(int *array, size_t size, int value);
+/* Auxiliar */
+int auxiliar_search(int array[], int start, int end, int key);
+ /* Advanced */
+int exponential_search(int *array, size_t size, int value);
 
 #endif
